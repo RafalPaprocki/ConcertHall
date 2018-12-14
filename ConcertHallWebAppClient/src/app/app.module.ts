b1c503e84fb47a6c3ef08298f7df0e84636af721
PaulPSta <pp.stabrawa@gmail.com>
2018-12-14 17:50:38 +0100

    Added register form

--- ConcertHallWebAppClient/src/app/app.module.ts
+++ ConcertHallWebAppClient/src/app/app.module.ts
@@ -2,31 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
-import { AppRoutingModule } from './app-routing.module';
-import { FormsModule } from '@angular/forms';
-import {BsModalService, ModalModule} from 'ngx-bootstrap/modal';
-import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
-import { AccordionModule } from 'ngx-bootstrap/accordion'
-import {ComponentLoaderFactory, PositioningService} from "ngx-bootstrap";
-import { ConcertHallMainViewComponent } from './components/concert-hall-main-view/concert-hall-main-view.component';
+import { NavComponent } from './nav/nav.component';
+import { RouterModule } from '@angular/router';
+import { appRoutes } from './routes';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ConcertHallMainViewComponent,
-  ],
-  imports: [
-    BrowserModule,
-     HttpClientModule,
-     AppRoutingModule,
-     FormsModule,
-    AccordionModule.forRoot(),
-    ModalModule.forRoot()
-  ],
-  providers: [
-    BsModalService,
-  ],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      NavComponent,
+      HomeComponent,
+      RegisterComponent
+   ],
+   imports: [
+      BrowserModule,
+      RouterModule.forRoot(appRoutes)
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ],
+   exports: [
+      RouterModule
+   ]
 })
 export class AppModule { }
---
+++ ConcertHallWebAppClient/src/app/register/register.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  @Output() cancelRegister = new EventEmitter();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  cancel() {
+    this.cancelRegister.emit(false);
+  }
+}
